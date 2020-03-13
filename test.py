8afcf4d9f668255bb3197962e9750ae0a6700348
Somashekharbk8 <62061492+Somashekharbk8@users.noreply.github.com>
2020-03-13 19:35:02 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+'''
+r = 3.2
+pi = 3.142
+a = pi*(r**2)
+print(a)
+print(int(a))
+
+a = 1.2
+b = 2
+c = a+b
+print(c)
+print(type(c))
+
+a = 8.444
+print(a)
+print(str(a))
+print((a))
+
+
+r = 3.8
+pi = 3.142
+a = 2*pi*r
+print(a)
+print(int(a))
+'''
+
+a = 5
+b = 5*12
+c = 13/100
+d = a*b*c
+print(d)
+print(int(d))
